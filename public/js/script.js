a74cafdc49683b6560936fd2f27efc0f84c9c979
Michelle Maltbia <maltbia.michelle@gmail.com>
2015-07-21 09:19:51 -0700

    Styling updates completed.

--- public/js/script.js
+++ public/js/script.js
@@ -176,6 +176,29 @@ function saveFile(filename, content) {
   }, errorHandler);
 }
 
+// Simple class example
+
+function SimpleSquareParticle(posX, posY) {
+		this.x = posX;
+		this.y = posY;
+		this.velX = 0;
+		this.velY = 0;
+		this.accelX = 0;
+		this.accelY = 0;
+		this.color = "#FF0000";
+		this.radius = 10;
+}
+
+//The function below returns a Boolean value representing whether the point with the coordinates supplied "hits" the particle.
+SimpleSquareParticle.prototype.hitTest = function(hitX,hitY) {
+	return((hitX > this.x - this.radius)&&(hitX < this.x + this.radius)&&(hitY > this.y - this.radius)&&(hitY < this.y + this.radius));
+}
+
+//A function for drawing the particle.
+SimpleSquareParticle.prototype.drawToContext = function(theContext) {
+	theContext.fillStyle = this.color;
+	theContext.fillRect(this.x - this.radius, this.y - this.radius, 2*this.radius, 2*this.radius);
+}
 
 function deleteFile(filename) {
   filesystem.root.getFile(filename, {create: false}, function(fileEntry) {
